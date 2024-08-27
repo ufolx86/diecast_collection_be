cdb42ebca84a2cab3bbe24cd2192d2f45162f6e1
ufolx86 <victor.r.urueta@gmail.com>
2024-08-27 22:27:21 +0200

    Routes, Server Side Validation and Controller

--- controllers/account.js
+++ controllers/account.js
@@ -1,7 +1,25 @@
+const { validationResult } = require('express-validator/check')
+
 exports.getProfile = (req, res, next) => {
   res.status(200).json({
     name: 'Test Name',
     lastname:'"Test Lastname',
     email: 'test@email.com'
+  })
+}
+
+exports.patchProfile = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Updating profile failed. Data is incorrect.', 
+      errors: errors.array()
+    })
+  }
+  const name = req.body.name;
+  const lastName= req.body.lastname;
+  res.status(201).json({
+    name: name,
+    lastName: lastName
   })
 }
--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator/check')
 
 const accountController = require('../controllers/account');
 const collectionController = require('../controllers/collection');
@@ -6,6 +7,10 @@ const collectionController = require('../controllers/collection');
 const router = express.Router();
 
 router.get('/profile', accountController.getProfile);
+// TODO: ADD PROPER VALIDATION, CHECK IF MATCHES WITH FRONT END VALIDATION
+router.patch('/profile', [
+  body('name').trim().isLength({min:5})
+], accountController.patchProfile);
 
 router.get('/collection', collectionController.getCollection);
 
